e4b4584ca5323937eaae81b15941086e544a2d56
Markus Waas <mail@markuswaas.de>
2020-03-05 21:34:21 +1300

    add gatsby google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,5 +72,20 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify-cms`,
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-TPGJ3Z4",
+  
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" }
+      },
+    },
   ],
 };
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,25 +5,12 @@ import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 export const replaceRenderer = ({
   bodyComponent,
   replaceBodyHTMLString,
-  setPostBodyComponents,
   setHeadComponents,
 }) => {
   const sheet = new ServerStyleSheet();
   const bodyHTML = renderToString(bodyComponent);
   replaceBodyHTMLString(bodyHTML);
   setHeadComponents([
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-TPGJ3Z4');</script>,
     sheet.getStyleElement(),
-  ]);
-  setPostBodyComponents([
-    <noscript>
-      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TPGJ3Z4"
-        height="0" width="0" style="display:none;visibility:hidden">
-      </iframe>
-    </noscript>
   ]);
 };
