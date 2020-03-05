d424b213adafec78356c507a7135fbca6aadf5dc
Markus Waas <mail@markuswaas.de>
2020-03-05 20:45:23 +1300

    fix mailchimp subscribe popup

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,5 +10,9 @@ export const replaceRenderer = ({
   const sheet = new ServerStyleSheet();
   const bodyHTML = renderToString(bodyComponent);
   replaceBodyHTMLString(bodyHTML);
-  setHeadComponents([sheet.getStyleElement()]);
+  setHeadComponents([
+    sheet.getStyleElement(),
+    <script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script>,
+    <script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us19.list-manage.com","uuid":"a407f3acc50091c0f47426c21","lid":"faec4da08d","uniqueMethods":true}) })</script>
+  ]);
 };
